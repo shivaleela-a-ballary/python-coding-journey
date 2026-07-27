d44ca72ba0b35a2c662cc946f7b3443d0a8e3410
Shivaleela A Ballary <bsleela8@gmail.com>
2026-07-27 15:33:33 +0530

    feat(udemy):solve right angle triangle patern

---
+++ Udemy/Right_Angled_triangle.py
@@ -0,0 +1,4 @@
+#You are given an integer n. Your task is to return a right-angled triangle pattern of '*' where each side has n characters, represented as a list of strings. The triangle has '*' characters, starting with 1 star in the first row, 2 stars in the second row, and so on until the last row has n stars.
+
+def generate_triangle(n):
+    return['*'*i for i in range (1,n+1)]
